ee883a2c8c798312f701db1f966276070fd84527
oliver-tbs <oliver.postlethwaite@tbs-sct.gc.ca>
2026-01-20 12:00:07 -0500

    program area column updates

--- src/webparts/spfxCmDetails/components/SpfxCmDetails.tsx
+++ src/webparts/spfxCmDetails/components/SpfxCmDetails.tsx
@@ -61,11 +61,19 @@ export default class SpfxCmDetails extends React.Component<ISpfxCmDetailsProps, 
     private env = {
         careerMarketplaceTermSetId: '656c725c-def6-46cd-86df-b51f1b22383e',
         jobTypeTermSetId: '45f37f08-3ff4-4d84-bf21-4a77ddffcf3e',
+        
         programAreaTermSetId: 'bd807536-d8e7-456b-aab0-fae3eecedd8a',
+        programAreaColumnName: 'Program_Area',
+        
         authClientId: 'c121f403-ff41-4db3-8426-f3b9c5016cd4',
         deleteApiUrl: 'https://appsvc-function-dev-cm-listmgmt-dotnet001.azurewebsites.net/api/DeleteJobOpportunity?',
         careerMarketplaceHomePage: 'https://devgcx.sharepoint.com/sites/CM-test',
         editOpportunityPage: 'https://devgcx.sharepoint.com/sites/CM-test/SitePages/editOpportunity.aspx?JobOpportunityId='
+
+
+
+
+
     }
 
     private envValid(): boolean {
@@ -179,7 +187,7 @@ export default class SpfxCmDetails extends React.Component<ISpfxCmDetailsProps, 
                 "ApplicationDeadlineDate", 
                 "ContactEmail", 
                 "ContactName", 
-                "ProgramArea", 
+                this.env.programAreaColumnName, 
                 "JobType", 
                 "Duration", 
                 "Duration/NameEn", 
@@ -262,14 +270,29 @@ export default class SpfxCmDetails extends React.Component<ISpfxCmDetailsProps, 
             }
 
             const expired = new Date() >= new Date(item.ApplicationDeadlineDate);
-            
+
+            let jobTypeTermGuid;
+            let programAreaTermGuid;
+
+            if (Array.isArray(item.JobType)) {
+                jobTypeTermGuid = item.JobType[0].TermGuid;
+            } else {
+                jobTypeTermGuid = item.JobType.TermGuid;
+            }
+
+            if (Array.isArray(item[this.env.programAreaColumnName])) {
+                programAreaTermGuid = item[this.env.programAreaColumnName][0].TermGuid;
+            } else {
+                programAreaTermGuid = item[this.env.programAreaColumnName].TermGuid;
+            }
+
             this.setState({
                 TitleFr: item.JobTitleFr,
                 TitleEn: item.JobTitleEn,
                 DescEn: item.JobDescriptionEn,
                 DescFr: item.JobDescriptionFr,
-                JobType: await this._get_terms(this.env.jobTypeTermSetId, item.JobType[0].TermGuid),
-                program: await this._get_terms(this.env.programAreaTermSetId, item.ProgramArea[0].TermGuid),
+                JobType: await this._get_terms(this.env.jobTypeTermSetId, jobTypeTermGuid),
+                program: await this._get_terms(this.env.programAreaTermSetId, programAreaTermGuid),
                 classification: `${item.ClassificationCode.NameEn}-${item.ClassificationLevel.NameEn}`,
                 Department: item.Department,
                 AppDeadline: item.ApplicationDeadlineDate.split('T')[0], // convert into format YYYY/MM/DD
